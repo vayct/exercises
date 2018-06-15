83ef0f44501847b09e22d9db4e56a3b8dbe487f0
vayct <vayct@slu.edu>
2018-06-14 21:31:29 -0500

    better demo template

---
+++ C/problems/demo.cpp
@@ -0,0 +1,146 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <cstdio>
+
+using namespace std;
+
+
+
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+
+#define ABS(x) ((x)<0?-(x):(x))
+#define pnl printf("\n")
+void swams(char *x, char *y) {char temp; temp=*x; *x=*y; *y=temp;}
+void swapi(int *a, int *b) {int temp; temp=*a;*a=*b;*b=temp;}
+
+
+
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+uint64 gcd(uint64 a,uint64 b){if(a==0)return b;if(b==0)return a;if(a==1||b==1)return 1;
+  if(a==b)return a;if(a>b)return gcd(b,a%b);else return gcd(a,b%a);}
+/****** Template of some basic operations *****/
+template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
+/**********************************************/
+
+/****** Template of Fast I/O Methods *********/
+template <typename T> inline void write(T x)
+{
+	int i = 20;
+	char buf[21];
+	// buf[10] = 0;
+	buf[20] = '\n';
+
+	do
+	{
+		buf[--i] = x % 10 + '0';
+		x/= 10;
+	}while(x);
+	do
+	{
+		putchar(buf[i]);
+	} while (buf[i++] != '\n');
+}
+
+template <typename T> inline T readInt()
+{
+	T n=0,s=1;
+	char p=getchar();
+	if(p=='-')
+		s=-1;
+	while((p<'0'||p>'9')&&p!=EOF&&p!='-')
+		p=getchar();
+	if(p=='-')
+		s=-1,p=getchar();
+	while(p>='0'&&p<='9') {
+		n = (n<< 3) + (n<< 1) + (p - '0');
+		p=getchar();
+	}
+
+	return n*s;
+}
+/************************************/
+
+
+/******** User-defined Function *******/
+
+
+/**************************************/
+
+
+/********** Main()  function **********/
+int main()
+{
+	//added two lines to make cin/cout fast as printf/scanf
+	//flushing stdout before stdin accepts an input
+	cin.tie(NULL);
+	//toggles off the synchronization of all the C++ standard streams
+	ios_base::sync_with_stdio(false);
+
+	int TC;
+	//for fast reading input
+	//   write(TC) is for writing number only
+	TC = read(int);
+
+	while(TC--){
+		pnl;
+
+	}
+	return 0;
+}
+/********  Main() Ends Here *************/
